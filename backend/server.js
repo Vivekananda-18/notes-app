b3c706981d5300262c1f000c99ab9fa6fc7b005a
vivek <vivekanandajoshi18@gmail.com>
2025-10-21 11:25:26 +0530

    Set CORS to allow all origins

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
-app.use(cors({
-  origin: "https://vivek-notes-app.netlify.app"
-}));
+app.use(cors());
 app.use(express.json());
 
 // --- Database Connection ---
